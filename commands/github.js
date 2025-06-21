56996091b72f77f7f2fd71f83bde5559b7cf33dd
dullar255 <dullahbest04@gmail.com>
2025-06-21 19:16:22 +0200

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -1,8 +1,8 @@
 async function githubCommand(sock, chatId) {
-    const repoInfo = `*🤖 SNOWBIRD XMD*
+    const repoInfo = `*🤖 DRAXEN Ai*
 
 *📂 GitHub Repository:*
-https://github.com/SNOWBIRD0074/SNOWBIRD-XMD-
+https://github.com/dullar255/DRAXEN-Ai
 
 *📢 Official Channel:*
 https://whatsapp.com/channel/0029Vb5nSebFy722d2NEeU3C
@@ -16,8 +16,8 @@ _Star ⭐ the repository if you like the bot!_`;
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363399707841760@newsletter',
-                    newsletterName: 'SNOWBORD XMD',
+                    newsletterJid: 'https://chat.whatsapp.com/Dg9rNdDl1HBJ12LnOmH0oD',
+                    newsletterName: 'DRAXEN Ai',
                     serverMessageId: -1
                 }
             }
