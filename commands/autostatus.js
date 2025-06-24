31a016641e9a2cf5e461ea3f2b4d4295b0bf95c6
dullar255 <dullahbest04@gmail.com>
2025-06-24 18:10:41 +0200

    Update autostatus.js

--- commands/autostatus.js
+++ commands/autostatus.js
@@ -6,8 +6,8 @@ const channelInfo = {
         forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterJid: '120363403345479886@newsletter',
+            newsletterName: 'Draxen Ai',
             serverMessageId: -1
         }
     }
