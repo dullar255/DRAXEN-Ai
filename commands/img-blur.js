04f95e190569d4f3c6262fcaf495e4514c6f272b
dullar255 <dullahbest04@gmail.com>
2025-06-24 18:44:30 +0200

    Update img-blur.js

--- commands/img-blur.js
+++ commands/img-blur.js
@@ -65,8 +65,8 @@ async function blurCommand(sock, chatId, message, quotedMessage) {
                 forwardingScore: 1,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterJid: '120363403345479886@newsletter',
+                    newsletterName: 'Draxen Ai',
                     serverMessageId: -1
                 }
             }
