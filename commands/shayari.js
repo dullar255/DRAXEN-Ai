aad872125c3219884458768924fc16a27d8947db
dullar255 <dullahbest04@gmail.com>
2025-06-24 19:04:44 +0200

    Update shayari.js

--- commands/shayari.js
+++ commands/shayari.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 async function shayariCommand(sock, chatId, message) {
     try {
-        const response = await fetch('https://api.shizo.top/api/quote/shayari?apikey=knightbot');
+        const response = await fetch('https://api.shizo.top/api/quote/shayari?apikey=draxenai');
         const data = await response.json();
         
         if (!data || !data.result) {
