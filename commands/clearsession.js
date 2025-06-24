c7c327bf33de698a8a3589013d1a006c91790d47
dullar255 <dullahbest04@gmail.com>
2025-06-24 18:20:58 +0200

    Update clearsession.js

--- commands/clearsession.js
+++ commands/clearsession.js
@@ -7,8 +7,8 @@ const channelInfo = {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterJid: '120363403345479886@newsletter',
+            newsletterName: 'Draxen Ai',
             serverMessageId: -1
         }
     }
